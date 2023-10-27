56b9e1b77abac3d22308710d3641c77da4f91950
reshma325 <138295298+reshma325@users.noreply.github.com>
2023-10-28 03:40:22 +0530

    update 28th Oct

--- Routes/Auth.routes.js
+++ Routes/Auth.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { login } from "../Controllers/Auth.controller.js";
+import { login,register } from "../Controllers/Auth.controller.js";
 
 const router = Router();
 router.get('/login',login )
+router.post("/register", register)
 
 export default router;
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import { Hello } from './Controllers/GlobalControllers.js';
 import router from './Routes/index.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import morgan from 'morgan';
 // import {Hello} from './'
 const app= express();
 
 
-app.get("/",function(req,res){
+dotenv.config()
+app.get("/",function(req,res,next){
     res.send("Hello from index.js")
+    next();
 
 })
 
 app.get("/Hello",Hello);
-
+app.use(morgan('dev'))
 app.use('/api/v1',router)
+
+
+mongoose.connect(process.env.MONGOURL).then(()=>console.log("Database Connected"))
 // app.get('/hello',Hello)
 app.listen(8000 ,()=>console.log('Hi'));
